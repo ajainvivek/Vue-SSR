09c618c906f9e44ac685145a6888b70488659d58
Ajain <ajainvivek07@gmail.com>
2018-03-22 18:15:43 +1100

    Add: routes config

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const config = require("./config")
 const isProduction = config.isProduction
 
 const template = fs.readFileSync(resolve("./client/index.template.html"), "utf-8")
+const Server = require("./server/index").default
+
+// Initialize Server
+new Server(app)
 
 const createRenderer = (bundle, options) => {
 	// https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
